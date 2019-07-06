8d8675fdef5691f78a6b22a873037057b57081f0
土井健 <doiken@doikennoMacBook-Pro.local>
2019-07-06 15:28:50 +0900

    api部分実装

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../service/httpservice.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public title = 'doiken';
+  public title2;
+  private get_url = 'https://httpbin.org/get?show_env=1';
+  private post_url = 'http://httpbin.org/post';
+
+  constructor( public httpService: HttpService){
+
+  }
+
+  ngOnInit() {
+    this.get();
+    console.log(this.title);
+  }
+  
+  async get() {
+    return await this.httpService.get(this.get_url).then(res => {this.title = res['args']; });
+  }
+
+  async post() {
+    return await this.httpService.post(this.post_url, JSON.stringify({'doiken': {'doiken': { 'doiken': 'god' } } })).then(res => {this.title2 = res['data']; });
+  }
+  
+
+  onClick(){
+    this.get();
+    console.log(this.title);
+    this.post();
+    console.log(this.title2);
+  }
 
 }
