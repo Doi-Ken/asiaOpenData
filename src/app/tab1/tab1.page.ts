4d2eeacffb275942ab43eafbf892bedcceeb74d7
土井健 <doiken@doikennoMacBook-Pro.local>
2019-07-06 19:28:32 +0900

    map表示

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,7 +10,9 @@ import { Observable } from 'rxjs';
 export class Tab1Page implements OnInit {
 
   public title;
-  private get_url = 'https://weather.api.here.com/weather/1.0/report.json?app_id=AnI4sryhVIB0pHljSuiK&app_code=Yhsj4Y4hKqfG1M71Y7OR8Q&product=observation&name=Tokyo';
+  private get_url = 'https://route.api.here.com/routing/7.2/calculateroute.json?app_id=yiVZ4u4SmBrttmo1yJkV&app_code=xLsB2C5DZitUTb7zcbs9pA&waypoint0=geo!52.513343,13.356054&waypoint1=geo!50.088478,14.432714&mode=fastest;car;traffic:disabled&alternatives=2&routeattributes=labels';
+  // https://route.api.here.com/routing/7.2/calculateroute.json?app_id=yiVZ4u4SmBrttmo1yJkV&app_code=xLsB2C5DZitUTb7zcbs9pA&waypoint0=geo!52.513343,13.356054&waypoint1=geo!50.088478,14.432714&mode=fastest;car;traffic:disabled&alternatives=2&routeattributes=labels
+  // https://weather.api.here.com/weather/1.0/report.json?app_id=AnI4sryhVIB0pHljSuiK&app_code=Yhsj4Y4hKqfG1M71Y7OR8Q&product=observation&name=Tokyo  
   private post_url = 'http://httpbin.org/post';
 
   constructor( public httpService: HttpService){
