f6fb32863bb12fbe2d3c25a3326ac6771af75006
Eigo Kainai <kainai@EigonoMacBook-Pro.local>
2019-07-07 11:54:25 +0900

    firebaseログイン機能追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,19 +11,28 @@ import { AppComponent } from './app.component';
 import { HttpService } from './service/httpservice.service';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { GeoService } from './service/geoService/geoservice.service';
+import { AuthService } from './service/auth/auth.service';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import * as firebase from 'firebase';
+import { ReactiveFormsModule } from '@angular/forms';
+firebase.initializeApp(environment.firebase);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-  HttpModule ],
+    AngularFireAuthModule, ReactiveFormsModule,
+    HttpModule,  ],
   providers: [
     HttpService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     Geolocation,
-    GeoService
+    GeoService,
+    AuthService,
   ],
   bootstrap: [AppComponent]
 })
